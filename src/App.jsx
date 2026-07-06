710cba418fe3c2f843049e6a7cf362fa439a2e23
manu601 <122957827+manu601@users.noreply.github.com>
2026-07-06 12:50:36 +0300

    Create first Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>Emmanuel Kipyegon</h1>
       <p>Welcome to my portfolio.</p>
     </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>Emmanuel Kipyegon</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
